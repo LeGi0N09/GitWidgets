a4a07cd62a88c470e1b013987bb61d32844e209f
⚡️Priyansh Modi <priyanshmodi9@gmail.com>
2026-03-20 23:31:01 +0530

    fix: add new tools, fix build config, skipLibCheck

--- public/src/widgets/skills.js
+++ public/src/widgets/skills.js
@@ -28,6 +28,7 @@ const themes_1 = __importDefault(require("../data/themes"));
  * @returns The SVG with all the skills that were passed.
  */
 function skillsWidget(languagesString, frameworksString, librariesString, toolsString, softwareString, includeNames, themeString) {
+    var _a;
     const BASE_HEIGHT = 125;
     const BASE_WIDTH = 812;
     const FIRST_ROW = 90;
@@ -49,13 +50,7 @@ function skillsWidget(languagesString, frameworksString, librariesString, toolsS
         softwareString = 'undefined';
     }
     // Set the theme
-    let theme = utils_1.getTheme(themes_1.default, 'default');
-    if (themeString) {
-        theme = utils_1.getTheme(themes_1.default, themeString);
-    }
-    if (!theme) {
-        theme = utils_1.getTheme(themes_1.default, 'default');
-    }
+    const theme = (_a = (themeString ? (0, utils_1.getTheme)(themes_1.default, themeString) : undefined)) !== null && _a !== void 0 ? _a : (0, utils_1.getTheme)(themes_1.default, 'default');
     const languageList = languagesString.split(',');
     const frameworkList = frameworksString.split(',');
     const libraryList = librariesString.split(',');
@@ -110,7 +105,7 @@ function skillsWidget(languagesString, frameworksString, librariesString, toolsS
         for (let i = 0; i < listToBuild.length; i++) {
             // Check the data and add the first result that isn't defined.
             // It checks the languages first, then the frameworks, and then the libraries.
-            let foundData = utils_1.findData(languages_1.default, listToBuild[i]) || utils_1.findData(frameworks_1.default, listToBuild[i]) || utils_1.findData(libraries_1.default, listToBuild[i]) || utils_1.findData(tools_1.default, listToBuild[i]) || utils_1.findData(software_ides_1.default, listToBuild[i]);
+            let foundData = (0, utils_1.findData)(languages_1.default, listToBuild[i]) || (0, utils_1.findData)(frameworks_1.default, listToBuild[i]) || (0, utils_1.findData)(libraries_1.default, listToBuild[i]) || (0, utils_1.findData)(tools_1.default, listToBuild[i]) || (0, utils_1.findData)(software_ides_1.default, listToBuild[i]);
             if (foundData === undefined) {
                 foundData = {
                     name: [''],
@@ -125,7 +120,7 @@ function skillsWidget(languagesString, frameworksString, librariesString, toolsS
             const row = Math.floor(i / 7);
             const transX = 102 * (i - row * 7);
             const transY = ROW * row + (includeNames && row > 0 ? 25 * row : 0);
-            boxes += gradient_box_1.default(
+            boxes += (0, gradient_box_1.default)(
             // Combine the index and the type number.
             (i * Math.pow(10, Math.floor(Math.log10(type)) + 1) + type), foundData.colorFrom, foundData.colorTo, transX, transY);
             boxes +=
@@ -149,7 +144,7 @@ function skillsWidget(languagesString, frameworksString, librariesString, toolsS
     return `
     <svg width="${width}" height="${height}" viewBox="0 0 ${width} ${height}"
     xmlns="http://www.w3.org/2000/svg">
-        ${card_1.default(width, height, theme.background)}
+        ${(0, card_1.default)(width, height, theme.background)}
         <g id="header-text" transform="translate(60 60)">
             <text id="skills" fill="${theme.title}" transform="translate(0 44)" font-size="42" font-family="Roboto-Medium, Roboto, sans-serif" font-weight="500">
                 <tspan x="0" y="0">Skills</tspan>
