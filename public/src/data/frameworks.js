1231ee0210ce44523a8c8476b0e6bbb412df93fc
⚡️Priyansh Modi <priyanshmodi9@gmail.com>
2026-03-21 11:42:04 +0530

    /

--- public/src/data/frameworks.js
+++ public/src/data/frameworks.js
@@ -250,6 +250,17 @@ const frameworks = [
         height: 27.961,
         xOffset: 0,
     },
+    {
+        name: ['FastAPI'],
+        colorFrom: '#A8FFE8',
+        colorTo: '#009688',
+        icon: `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width="36" height="36">
+        <path fill="#FFFFFF" d="M12 0C5.375 0 0 5.375 0 12c0 6.627 5.375 12 12 12 6.626 0 12-5.373 12-12 0-6.625-5.373-12-12-12zm-.624 21.62v-7.528H7.19L13.203 2.38v7.528h4.029L11.376 21.62z"/>
+        </svg>`,
+        width: 36,
+        height: 36,
+        xOffset: 0,
+    },
 ];
 exports.default = frameworks;
 //# sourceMappingURL=frameworks.js.map
